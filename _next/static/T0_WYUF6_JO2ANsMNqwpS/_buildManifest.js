e10f54b29e40ddc424027cac708298488c7cd95f
zih-an <zihanzhao777@gmail.com>
2025-12-22 23:13:39 +0800

    Deploy project page

--- _next/static/T0_WYUF6_JO2ANsMNqwpS/_buildManifest.js
+++ _next/static/T0_WYUF6_JO2ANsMNqwpS/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/offsetcrust//_next/:path+",
+        "destination": "/offsetcrust/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
